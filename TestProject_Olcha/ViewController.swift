81e1f593a94d1a1ee7ccf6dc591fd08a867b1476
Sarvar <sarvarqosimov094@gmail.com>
2024-03-01 16:09:35 +0500

    Initial Commit

---
+++ TestProject_Olcha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject_Olcha
+//
+//  Created by Sarvar Qosimov on 01/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
